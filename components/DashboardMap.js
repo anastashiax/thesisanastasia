7a1615c8ac1c7eadcb1ca2f812df19e6132abf11
Anastasia Aurelia <aurelanas@gmail.com>
2021-11-11 14:32:38 +0700

    Changed the Height

--- components/DashboardMap.js
+++ components/DashboardMap.js
@@ -11,7 +11,7 @@ const DashboardMap = ({ properties }) => {
   console.log(properties[0].location?.lat)
   const containerStyle = {
     width: "100%",
-    height: "100vh",
+    height: "210vh",
   }
 
   const center = {
